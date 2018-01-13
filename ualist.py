2be46a7f4132001686e95fd0706b26555133ec53
tz1006 <a@omg.tf>
2018-01-13 00:04:23 -0800

    Update ualist.py

--- ualist.py
+++ ualist.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*- 
-# filename: ua_list.py
+# filename: ctrip/ua_list.py
 
 ua_list = [
     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
